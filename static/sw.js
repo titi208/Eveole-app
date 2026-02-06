598c12e48eb110162d954deb9da87a2217a1d129
Labrisure <79997197+titi208@users.noreply.github.com>
2026-02-06 18:39:28 +0100

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'eveole-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/icon.png',
+  '/static/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
